1b787e2f4f16af4679f0d41028c268a9bf70adbf
Dylan Zarn <zarndy77@gmail.com>
2025-05-14 23:46:08 -0500

    Offloaded course retrieval and calculated variables to CourseService and ScoringService

--- HoleOut/Models/Round.swift
+++ HoleOut/Models/Round.swift
@@ -19,32 +19,6 @@ final class Round {
     var scores: [Int]
     var playedHoles: [Bool]
     
-    var course: Course {
-            // First try to pull by ID
-            if let course = CourseRepository.shared.getCourse(byId: courseId) {
-                return course
-            }
-            
-            // If ID lookup fails, try by name
-            if let course = CourseRepository.shared.getCourse(byName: courseName) {
-                // Update the ID for future reference
-                courseId = course.id
-                return course
-            }
-            
-            // Log error and return a default course (first course, St. Boniface) as last resort
-            let logger = Logger(origin: "Round")
-            logger.log("Failed to find course with ID \(courseId) or name \(courseName)", level: .error)
-            
-            let defaultCourse = CourseRepository.shared.getAllCourses()[0]
-            courseId = defaultCourse.id
-            courseName = defaultCourse.name
-            
-            return defaultCourse
-        }
-    
-    
-
     init(at course: Course) {
         self.id = UUID()
         self.courseId = course.id
@@ -58,46 +32,9 @@ final class Round {
     
     // MARK: - Computed properties
     
-    /// computes total score for holes played
-    var totalScore: Int {
-        zip(scores, playedHoles)
-            .filter {_, played in played}
-            .map { score, _ in score }
-            .reduce(0,+)
-    }
-    
-    /// computes the par for the holes played
-    var parForPlayedHoles: Int {
-        let total = zip(course.holes, playedHoles)
-            .filter {_, played in played }
-            .map { hole, _ in
-                print("Counting par \(hole.par) for hole \(hole.id)")
-                return hole.par
-            }
-            .reduce(0,+)
-        print("Total par for played holes: \(total)")
-            return total
-    }
-    
-    /// computes score on the front nine
-    var frontNine: Int {
-        zip(scores.prefix(9), playedHoles.prefix(9))
-            .filter { _, played in played }
-            .map { score, _ in score }
-            .reduce(0,+)
-    }
-    
-    /// computes score on the back nine
-    var backNine: Int {
-        zip(scores.suffix(9), playedHoles.suffix(9))
-            .filter { _, played in played }
-            .map { score, _ in score }
-            .reduce(0,+)
-    }
-    
     /// Checks if all 18 holes were played
     var isComplete: Bool {
-        holesPlayed == course.holes.count
+        holesPlayed == playedHoles.count
     }
     
     /// checks if at least 1 hole and less than 18 were played
