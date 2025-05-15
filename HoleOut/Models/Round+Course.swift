55a55e97ecaaf8add765e51e3af6e64390328e24
Dylan Zarn <zarndy77@gmail.com>
2025-05-15 00:05:54 -0500

    Extension for course retrieval to prevent storing courses in round models

--- HoleOut/Models/Round+Course.swift
+++ HoleOut/Models/Round+Course.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Round {
+    /// Gets the course associated with this round
+    /// - Parameter courseService: The course service to use
+    /// - Returns: The course for this round
+    func getCourse(using courseService: CourseService) -> Course {
+        return courseService.getCourseForRound(self)
+    }
+}
